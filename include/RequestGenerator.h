6542950f7adf1dd0577f34c48b05193f236e522a
Donn <donnspersonal@icloud.com>
2018-11-08 14:39:26 +0200

    Reqgen communication

--- include/RequestGenerator.h
+++ include/RequestGenerator.h
@@ -2,7 +2,7 @@
 #define _request_generator_h
 
 #include "Message.h"
-
+#include "Connection.h"
 #include <string>
 
 namespace RRAD {
@@ -11,8 +11,9 @@ namespace RRAD {
         uint64 requestCounter = 0;
     public:
         RequestGenerator(std::string userName): userName(userName) {}
-        Message requestList(std::string className, std::string targetUser);
-        Message requestRMI(std::string className, std::string targetUser, JSON id, std::string method, JSON arguments);
+        Message listRPC(std::string className, std::string targetUser);
+        Message rmi(std::string className, std::string targetUser, JSON id, std::string method, JSON arguments);
+        JSON communicateRMI(std::string targetIP, uint16 targetPort, Message rmi);
         std::string getUserName() { return userName; }
     };
 }
--- src/RequestGenerator.cpp
+++ src/RequestGenerator.cpp
@@ -2,7 +2,7 @@
 
 #include "Dispatcher.h"
 
-RRAD::Message RRAD::RequestGenerator::requestList(std::string className, std::string targetUser) {
+RRAD::Message RRAD::RequestGenerator::listRPC(std::string className, std::string targetUser) {
     auto message = RRAD::Message();
     message.msg_json["senderID"] = userName;
     message.msg_json["receiverID"] = targetUser;
@@ -13,10 +13,17 @@ RRAD::Message RRAD::RequestGenerator::requestList(std::string className, std::st
     return message;
 }
 
-RRAD::Message RRAD::RequestGenerator::requestRMI(std::string className, std::string targetUser, JSON id, std::string method, JSON arguments) {
-    auto message = requestList(className, targetUser);
+RRAD::Message RRAD::RequestGenerator::rmi(std::string className, std::string targetUser, JSON id, std::string method, JSON arguments) {
+    auto message = listRPC(className, targetUser);
     message.msg_json["object"] = id;
     message.msg_json["operation"]["name"] = method;
     message.msg_json["operation"]["arguments"] = arguments;
     return message;
+}
+
+JSON RRAD::RequestGenerator::communicateRMI(std::string targetIP, uint16 port, RRAD::Message rmi) {
+    auto conn = RRAD::Connection(targetIP, port);
+    conn.write(rmi.marshall());
+    auto reply = RRAD::Message::unmarshall(conn.read());
+    return reply.getArguments();
 }
