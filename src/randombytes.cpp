8539370dc1ac2ebfde0b0beec56dfe81d0416190
Mohamed Gaber <donnspersonal@icloud.com>
2018-11-09 19:05:32 +0200

    Fixed source of entropy

--- src/randombytes.cpp
+++ src/randombytes.cpp
@@ -3,10 +3,10 @@
 #include <random> 
 extern "C" {
     // For TweetNACL
-    void randombytes(uint8* buffer, uint64 count) {
+    void randombytes(unsigned char* buffer, unsigned long long count) {
         static std::random_device generator;
-        static std::uniform_int_distribution<uint8> distribution(0,255);
-        for (uint64 i = 0; i < count; i += 1) {
+        static std::uniform_int_distribution<unsigned char> distribution(0,255);
+        for (unsigned long long i = 0; i < count; i += 1) {
             buffer[i] = distribution(generator);
         }
     }
